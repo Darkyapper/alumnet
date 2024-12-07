a7b5d3d04b416fcf65e7f2a7eae78eca77dfafd2
David Chay <yapperdark2@gmail.com>
2024-12-07 12:38:43 -0600

    add navigation to login form in LandingNavbar component

--- src/components/landingNavbar/LandingNavbar.jsx
+++ src/components/landingNavbar/LandingNavbar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function LandingNavbar() {
+
+  const navigate = useNavigate();
+
+  const handleNavigateToLoginForm = () => {
+    navigate("/login");
+  };
+
   return (
     <nav className="bg-[#34B5F6] text-white px-6 py-4">
       <div className="container mx-auto flex items-center justify-between">
@@ -10,7 +18,8 @@ function LandingNavbar() {
         </div>
 
         {/* Call-to-Action Button */}
-        <button className="transition ease-in-out raleway-font bg-[#FFF1E6] text-[#00032E] px-4 py-2 rounded-lg font-bold hover:bg-[#EDDFD4] hover:scale-105">
+        <button className="transition ease-in-out raleway-font bg-[#FFF1E6] text-[#00032E] px-4 py-2 rounded-lg font-bold hover:bg-[#EDDFD4] hover:scale-105"
+        onClick={handleNavigateToLoginForm}>
           Iniciar Sesión
         </button>
       </div>
